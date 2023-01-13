64ace2a38aa0f0f81cb4032b980612361a3dacd9
Adam Yala <adamkyala@gmail.com>
2023-01-13 10:04:13 -0700

    switch to stub label argument

--- config/modal_wsgi.py
+++ config/modal_wsgi.py
@@ -7,14 +7,13 @@ from . import image, mounts, secrets
 
 # the name parameter passed to Stub will be the name of the app
 # in the modal dashboard
-stub = modal.Stub(name="web")
+stub = modal.Stub(name='web')
 
 
-@stub.wsgi(image=image, secret=secrets, mounts=mounts)
+@stub.wsgi(label='django-example', image=image, secret=secrets, mounts=mounts)
 def app():
-    # the name of this function will be appended to the url
-    # a stub name of "web" and a function name of "app" will
-    # result in the url https://<workspace name>--web-app.modal.run
+    # the label argument of this function will be part of the url
+    # this label will result in https://<workspace name>--django-example.modal.run
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.modal_settings")
 
